7ed0c459e8f9188a14f34d36b5e6e32df769c436
dotndev <christelle.canovas@gmail.com>
2020-02-03 15:30:02 +0100

    Updated comments in files

--- app/src/main/java/androidkotlin/canovasc/todoapp/MainActivity.java
+++ app/src/main/java/androidkotlin/canovasc/todoapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import androidkotlin.canovasc.todoapp.Utils.FileManager;
@@ -44,33 +45,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    //Check if existing file
-
-    //Create new file
-
-    //Load existing file
-
-    //Display List
-
-    //Add new task to Array
-    //Add new task to File
-    //Refresh List
-
-    //Delete task from file
-    //Delete task from Array
-    //Refresh List
-
-
+    //Display RecyclerView if tasks already exist
     public void setRecyclerView(){
+
         mTaskArrayList = FileManager.loadTasks(this);
+
+        //Check if file already exists
         if(!mTaskArrayList.isEmpty()){
+            isFirstTask = false;
             mTaskAdapter = new TaskAdapter(mTaskArrayList,this,this);
             mRecyclerView = findViewById(R.id.recyclerview);
             mRecyclerView.setHasFixedSize(false);
             mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
             mRecyclerView.setAdapter(mTaskAdapter);
+
+            //Initialise the swipe-to-delete
             ItemTouchHelper.SimpleCallback itemTouchHelper = new RecyclerItemTouchHelper(0,ItemTouchHelper.LEFT,this);
             new ItemTouchHelper(itemTouchHelper).attachToRecyclerView(mRecyclerView);
+
         }else{
             isFirstTask = true;
         }
@@ -78,20 +70,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-
+    //Swipe-to-delete on existing task
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
 
+        Task task = mTaskArrayList.get(viewHolder.getAdapterPosition());
+        FileManager.deleteTask(this,task);
+        mTaskArrayList.remove(viewHolder.getAdapterPosition());
+        mTaskAdapter.notifyDataSetChanged();
+
     }
 
-    //Click on Task
+    //Click on existing task
     @Override
     public void onTaskClick(int position) {
 
     }
 
 
-    //Click on save button
+    //Click on + icon
     @Override
     public void onClick(View view) {
 
